688841437cac81c2e1ad2c332e07c6ab75ec54c5
Captain <44041524+canaimax@users.noreply.github.com>
2019-02-04 18:44:49 -0800

    Update musicView.php

--- view/musicView.php
+++ view/musicView.php
@@ -1,6 +1,28 @@
+
+<head>
+    <script src="action.js"></script>
+  <link rel="stylesheet" type="text/css" href="style.css">
+  <title>GROUP 5</title>
+</head>
+<body bgcolor=black>
+<form action="/LINK/index.html" method="get">
+        <div class="grid-container-top">
+             <div class="item2">
+        <img src="concert.png" alt="concert">
+             </div>
+        </div>
+
+        <div class="grid-container">
+             <div class="item3">
+                 <h1>MUSIC PLANET</h1>
+
+              </div>
+        </div>
+</div>
 <?php
 
-
+echo "<div style ='background-color:black;font:34px/55px Arial,tahoma,sans-serif;color:darkkhaki;text-align:left'>";
+echo "<table>";
 $servername = "localhost:3307";
 $username = "root";
 $password = "root";
@@ -10,29 +32,23 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-
 $sql = "SELECT Name, Artist, Year FROM Songs";
 $result = $conn->query($sql);
-echo "<font size=\"12\" face='Arial'>";
-echo "<table>";
-echo "&nbsp;&nbsp;&nbsp;&nbsp;NAME&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-ARTIST&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;YEAR" . "<br>";
+
+echo "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;NAME&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+ARTIST&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;YEAR" . "<br>";
 
 if ($result->num_rows > 0) {
-    // output data of each row
-	//echo $data1 . '\t' . $data2 . '\t' . $data3 . '\n';
+
     while($row = $result->fetch_assoc()) {
-        echo "&nbsp;&nbsp;&nbsp;&nbsp;" . $row["Name"].
-		"&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;" . $row["Artist"].
-		"&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;" .
-		$row["Year"]. "<br>";
+    echo "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;" . $row["Name"] . "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;" . $row["Artist"].
+        "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;" .
+        $row["Year"]. "<br>";
     }
 } else {
     echo "0 results";
 }
 $conn->close();
-
 echo "</table>";
-echo "</font>"; 
-
+echo "</font>";
 ?>
