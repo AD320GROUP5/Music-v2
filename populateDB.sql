0e9845aa593d1773d687966dc87b8a10bf41ef6a
Captain <44041524+canaimax@users.noreply.github.com>
2019-02-07 15:00:20 -0800

    Add files via upload

---
+++ populateDB.sql
@@ -0,0 +1,6 @@
+
+INSERT INTO songs VALUES
+('Wide Awake', 'Katty Perry', '2007', 'Pop'),
+('Hey Jude', 'The Beatles', '1967', 'Pop'),
+('Imagine', 'John Lennon', '1994', 'Pop'),
+('The Seasons', 'Vivaldi', '1784', 'Classic');
