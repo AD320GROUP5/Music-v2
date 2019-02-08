dd9cb0c0dd37999924ac4d710714b101d2d02de2
Captain <44041524+canaimax@users.noreply.github.com>
2019-02-07 18:28:29 -0800

    Add files via upload

---
+++ LINK/action.js
@@ -0,0 +1,21 @@
+function addClick(){
+window.location.href = '/ADD/index.html';	
+	
+// alert("Adding a Song");
+}
+
+function deleteClick(){
+window.location.href = '/DELETE/index.html';
+}
+
+
+function editClick(){
+window.location.href = '/EDIT/index.html';
+}
+
+
+function viewClick(){
+window.location.href = '/VIEW/musicView.php';	
+}
+
+
