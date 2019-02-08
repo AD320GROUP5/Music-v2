4903c22de5c95f5378cd87ff4a21eca910c0a5d7
Captain <44041524+canaimax@users.noreply.github.com>
2019-02-07 18:31:11 -0800

    Add files via upload

---
+++ LINK/populateDB.sql
@@ -0,0 +1,8 @@
+
+use musicplanet;
+
+INSERT INTO songs VALUES
+('Wide Awake', 'Katty Perry', '2007', 'Pop'),
+('Hey Jude', 'The Beatles', '1967', 'Pop'),
+('Imagine', 'John Lennon', '1994', 'Pop'),
+('The Seasons', 'Vivaldi', '1784', 'Classic');
